12c96e00e4a52b484f0b28d561e187e6e5017593
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-03-08 02:08:14 -0500

    Fourteenth chapter

--- fift.exs
+++ fift.exs
@@ -64,3 +64,93 @@ end
 
 # Multi alias/import/require/use
 alias React.{Component, CreateElement}
+
+# -----------------------------------------------------------------------------
+
+# Module attributes
+# They serve for three purposes:
+# 1. They serve to annotate the module, often with information
+#    to be used by the user or the VM.
+# 2. They work as constants.
+# 3. They work as a temporary module storage to use during compilation.
+
+# As annotations
+defmodule MyApp do
+  @moduledoc """
+  # MyApp module documentation
+    Provides a module with a description.
+    ```
+      iex> MyApp.hello
+      "Hello, world!"
+    ```
+  """
+  # Elixir promotes the use of Markdown with heredocs.
+  @doc "This is a function documentation."
+  def hello do
+    "Hello, world!"
+  end
+end
+# The @moduledoc and @doc are by far the most used attributes.
+# If you execute "h MyApp.hello" in the Erlang shell,
+# it will print "This is a function documentation."
+
+# As constants
+defmodule MyApp do
+  @constant "This is a constant."
+  IO.puts @constant
+  # This will print "This is a constant."
+end
+# They must be defined before trying to access them.
+
+defmodule MyApp.Status do
+  @service URI.parse("https://example.com")
+  def status(email) do
+    ServiceHTTP.get(@service)
+  end
+end
+# The @service function is called at compilation time,
+# and its value is then substituted in ServiceHTTP.get function.
+# ServiceHTTP.get(%URI{
+#   authority: "example.com",
+#   host: "example.com",
+#   port: 443,
+#   scheme: "https"
+# })
+# This is useful when using pre-computed values. But can be problematic
+# if you expect the value to be computed at runtime.
+# Elixir takes a snapshot of an attribute every time it is read inside a function.
+# So if the value is read multiple times, it will produce multiple copies of it.
+# This is a problem when the values are heavy to compute.
+
+# So, instead of this:
+def some_func, do: something_do(@value)
+def other_func, do: something_else(@value)
+# You can do this:
+def some_func, do: something_do(values())
+def other_func, do: something_else(values())
+defp values, do: @value
+
+# Acummulating Attributes
+defmodule App do
+  Module.register_attribute(__MODULE__, :api, accumulate: true)
+
+  @api :some
+  @api :other
+  # @api = [:some, :other]
+end
+
+# As temporary storage
+defmodule MyTest do
+  use ExUnit.TestCase, async: true
+
+  @test :external
+  @test os: :unix
+  test "contacts external service" do
+    @api = ServiceHTTP.get(@service)
+    assert @api.ok?
+  end
+end
+# In the above example, the async attribute is stored in a module attribute,
+# to change how the module is compiled.
+
+# -----------------------------------------------------------------------------
