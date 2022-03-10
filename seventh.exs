26b398a2351951b21e11591cdf18deacf4b7e5ef
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-03-10 00:23:02 -0500

    Twentieth chapter

--- seventh.exs
+++ seventh.exs
@@ -80,6 +80,7 @@ try do
 after
   File.close(file)
 end
+
 # The after clause will be executed even if the try clause failed.
 # However, after only provides a soft guarantee, not getting run
 # when linked process exits.
@@ -91,6 +92,7 @@ defmodule Util do
     IO.puts("Done")
   end
 end
+
 # Elixir allows you use rescue/after/catch in functions, without
 # having to wrap them in a try construct.
 
@@ -98,6 +100,7 @@ end
 # To match the results of the try clause when it finishes whitout
 # a throw or an error.
 x = 2
+
 try do
   1 / x
 rescue
@@ -106,6 +109,76 @@ else
   y when y < 1 and y > -1 -> :small
   _ -> :large
 end
+
 # returns :small
 
+# ------------------------------------------------------------------------------
 
+# Types and Specs
+# Being dynamic, Elixir check the types of variables at runtime.
+# Thus, it comes with typespecs which are used for:
+# 1. Declaring typed functions signatures
+# 2. Declaring custom types
+
+# Function signatures
+# Functions specs are written with @spec
+round(number()) :: integer()
+# The syntax is: function_name(arg1, arg2, ...) :: return_type
+# Return types may omit parentheses.
+
+# Defining Custom Types
+# It can help communicate the intention of the code, and make it easier
+# to understand.
+defmodule MyDate do
+  @typedoc """
+  A regional formatted date.
+  """
+  @type date :: {integer(), integer(), integer()}
+
+  @spec now() :: date
+  def now() do
+    %Date{day: d, month: m, year: y} = Date.utc_today()
+    "#{d}/#{m}/#{y}"
+  end
+end
+
+# Behaviours
+# They provide a way to define and ensure that functions of a specific
+# set will be implemented by a given module.
+# Similar to Interfaces in other languages.
+defmodule Parser do
+  @doc "Parses a string"
+  @callback parse(String.t()) :: {:ok, term()} | {:error, String.t()}
+
+  @doc "List supported file extensions"
+  @callback extensions() :: [String.t()]
+end
+
+# Modules that implement the Parser behaviour will have to implement
+# both the parse and the extensions functions.
+# If a module doesn't implement a function, a warning will be raised.
+defmodule JSONParser do
+  @behaviour Parser
+
+  @impl Parser
+  def parse(string), do: {:ok, "Parsed JSON" <> string}
+
+  @impl Parser
+  def extensions(), do: ["json"]
+end
+
+# Dynamic Dispatch
+# Behaviours and them often go hand in hand.
+defmodule Parser do
+  @callback parse(String.t()) :: {:ok, term()} | {:error, String.t()}
+  @callback extensions() :: [String.t()]
+
+  def parse!(implementation, contents) do
+    case implementation.parse(contents) do
+      {:ok, data} -> data
+      {:error, message} -> raise ArgumentError, "Error parsing: #{message}"
+    end
+  end
+end
+# The function parse! will dispatch to the given parse implementation
+# and return the result or raise an exception if case of error.
